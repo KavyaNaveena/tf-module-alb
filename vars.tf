ff2cf536f92d8ce6752d6a874896fd234331a4da
Kavya Naveena <karumurikavya@gmail.com>
2023-04-11 10:29:42 +0530

    update

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "name" {}
+variable "env" {}
+variable "tags" {}
+variable "internal" {}
+variable "load_balancer_type" {}
+variable "subnets" {}
+
+variable "enable_deletion_protection" {
+  default = false
+}
