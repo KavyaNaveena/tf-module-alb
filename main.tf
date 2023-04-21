37355d57115e0cefef52d7c793235d40ce4a0f17
kavya <karumurikavya@gmail.com>
2023-04-21 10:55:00 +0530

    yes

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_lb" "main" {
   internal           =  var.internal
   load_balancer_type =  var.load_balancer_type
   subnets            =  var.subnets
+  security_groups    =  [aws_security_group.main.id]
 
   enable_deletion_protection = var.enable_deletion_protection
 
@@ -13,38 +14,30 @@ resource "aws_lb" "main" {
   )
 }
 
-#resource "aws_security_group" "main" {
-#  name        = "${var.component}-${var.env}"
-#  description = "${var.component}-${var.env}"
-#  vpc_id      =  var.vpc_id
-#
-#  ingress {
-#    description      = "ssh"
-#    from_port        = 22
-#    to_port          = 22
-#    protocol         = "tcp"
-#    cidr_blocks      = var.bastion_cidr
-#  }
-#
-#  ingress {
-#    description      = "APP"
-#    from_port        = var.port
-#    to_port          = var.port
-#    protocol         = "tcp"
-#    cidr_blocks      = var.allow_app_to
-#  }
-#
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = merge(
-#    var.tags,
-#    { Name = "${var.component}-${var.env}" }
-#  )
-#}
+resource "aws_security_group" "main" {
+  name        = "${var.name}-${var.env}-alb"
+  description = "${var.name}-${var.env}-alb"
+  vpc_id      =  var.vpc_id
+
+  ingress {
+    description      = "APP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.name}-${var.env}-alb" }
+  )
+}
--- vars.tf
+++ vars.tf
@@ -4,6 +4,8 @@ variable "tags" {}
 variable "internal" {}
 variable "load_balancer_type" {}
 variable "subnets" {}
+variable "vpc_id" {}
+variable "allow_cidr" {}
 
 
 variable "enable_deletion_protection" {
